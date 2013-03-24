405f0abea4c5f2491fe9633386e4d247a2637698
cllmhl <private>
2013-03-24 12:27:52 +0100

    Using parent home for modules.

--- src/main/java/it/fe/cllmhl/Installation.java
+++ src/main/java/it/fe/cllmhl/Installation.java
@@ -7,26 +7,35 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 public final class Installation {
-    
+
     private static java.util.logging.Logger mLogger = java.util.logging.Logger.getLogger(Installation.class.getName());
-    
+
     private static String home = null;
     static {
-        //Standard environment first guess
+        // Standard environment first guess
         File lFileHomeDirectory = new File("home");
         if (lFileHomeDirectory.exists()) {
             home = lFileHomeDirectory.getAbsolutePath().concat(File.separator);
-            mLogger.info("Application home: "+home);
+            mLogger.info("Application home: " + home);
         }
-        //Standard environment second guess
+        // Standard environment second guess
         if (home == null) {
-            lFileHomeDirectory = new File(System.getProperty("user.dir")+File.separator+"home");
+            // Parent! I'm a module!
+            lFileHomeDirectory = new File("../home");
             if (lFileHomeDirectory.exists()) {
                 home = lFileHomeDirectory.getAbsolutePath().concat(File.separator);
-                mLogger.info("Application home: "+home);
+                mLogger.info("Application home: " + home);
             }
         }
-        //Container
+        // Standard environment second guess
+        if (home == null) {
+            lFileHomeDirectory = new File(System.getProperty("user.dir") + File.separator + "home");
+            if (lFileHomeDirectory.exists()) {
+                home = lFileHomeDirectory.getAbsolutePath().concat(File.separator);
+                mLogger.info("Application home: " + home);
+            }
+        }
+        // Container
         if (home == null) {
             try {
                 Context lContext = (Context) new InitialContext().lookup("java:comp/env");
@@ -38,14 +47,20 @@ public final class Installation {
             mLogger.info("FATAL ERROR!!! Unable to establish an Application home directory");
         }
     }
+
     public static String getConfigurationDirectory() {
-        return(home.concat(File.separator).concat("conf").concat(File.separator));
+        return (home.concat(File.separator).concat("conf").concat(File.separator));
+    }
+
+    public static String getHomeDirectory() {
+        return home;
     }
 
     public static String getTempDirectory() {
-        return(home.concat(File.separator).concat("temp").concat(File.separator));
+        return (home.concat(File.separator).concat("temp").concat(File.separator));
     }
-    
-    //Utility class
-    private Installation(){}
+
+    // Utility class
+    private Installation() {
+    }
 }
